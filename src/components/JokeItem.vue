360a6d12207552d31bc60c2a0b17422f369e3af7
psulli10 <petersullivan1985@gmail.com>
2019-06-21 14:29:50 +0100

    Added button to show/hide punchline.

--- src/components/JokeItem.vue
+++ src/components/JokeItem.vue
@@ -1,13 +1,33 @@
 <template lang="html">
   <div class="">
     <h4>{{joke.setup}}</h4>
+    <button v-on:click="handleClick" type="button" name="punchline-button">Make me laugh</button>
+    <h4 v-if="showPunchline">{{joke.punchline}}</h4>
   </div>
 </template>
 
 <script>
 export default {
   name:"joke-item",
-  props: ['joke']
+  props: ['joke'],
+
+  data(){
+    return {
+      showPunchline: false
+    }
+  },
+
+
+  methods: {
+    handleClick(){
+      if (this.showPunchline === false){
+          this.showPunchline = true;
+      }
+      else if(this.showPunchline === true){
+          this.showPunchline = false;
+      }
+    }
+  },
 }
 </script>
 
